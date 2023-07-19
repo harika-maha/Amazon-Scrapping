61b3a67cd14c34f0864c4ecdde4d999c841e2a6a
varshacb <83687662+varshacb@users.noreply.github.com>
2023-07-19 19:27:52 +0530

    chk commit

--- app.py
+++ app.py
@@ -7,15 +7,16 @@ driver.get("https://www.amazon.com/")
 
 
 
-search_query=driver.find_element(By.XPATH,'//input[@ID="twotabsearchtextbox"]')
-search_query.send_keys("mac book")
-# #locate Google Search button by _xpath code 3
-search_btn =driver.find_element(By.XPATH,'//input[@ID="nav-search-submit-button"]')
-# # use submit() to mimic enter key code 4
-search_btn.submit()
-urltag=driver.find_element(By.XPATH,'//a[@CLASS="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"]')
-actualurl=urltag.get_attribute('href')
-print(actualurl)
+# search_query=driver.find_element(By.XPATH,'//input[@ID="twotabsearchtextbox"]')
+# search_query.send_keys("macbook")
+# search_btn =driver.find_element(By.XPATH,'//input[@ID="nav-search-submit-button"]')
+# search_btn.submit()
+# urltag=driver.find_element(By.XPATH,'//a[@CLASS="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"]')
+# actualurl=urltag.get_attribute('href')
+# driver.get(actualurl)
+# price=driver.find_element(By.XPATH,'//span[@CLASS="a-price-whole"]')
+# print("heyyyyy")
+# print ("hey"+price)
 
 # driver.get(actualurl)
-# time.sleep(5)#wait for page load complete
+time.sleep(25)#wait for page load complete
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+
+# search_query=driver.find_element(By.XPATH,'//input[@ID="twotabsearchtextbox"]')
+# search_query.send_keys("macbook")
+# search_btn =driver.find_element(By.XPATH,'//input[@ID="nav-search-submit-button"]')
+# search_btn.submit()
+# urltag=driver.find_element(By.XPATH,'//a[@CLASS="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"]')
+# actualurl=urltag.get_attribute('href')
+# driver.get(actualurl)
