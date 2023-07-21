1dc98dbe9f07df54f46c83926186f8d9540124c2
Harika <lakshmimaha683@gmail.com>
2023-07-21 13:58:55 +0530

    ebay done

--- helper.py
+++ helper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup as bs
 import requests as r
+import regex as re
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -16,7 +17,7 @@ def flipkart(searchData):
 
     driver.get("https://www.flipkart.com/")
 
-    time.sleep(5)
+    time.sleep(6)
 
     search=driver.find_element(By.XPATH,'//*[@id="container"]/div/div[1]/div[1]/div[2]/div[2]/form/div/div/input')
     time.sleep(8)
@@ -43,3 +44,46 @@ def flipkart(searchData):
     print(price)
 
 # flipkart("macbook")
+
+def ebay(searchData):
+    options =Options()
+
+    # options.add_argument("incognito")
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver=webdriver.Chrome(options=options)
+    driver.get("https://www.ebay.com/")
+
+    time.sleep(5)
+
+    search=driver.find_element(By.XPATH,'//input[@CLASS="gh-tb ui-autocomplete-input"]')
+    time.sleep(5)
+    search.send_keys("macbook")
+    time.sleep(5)
+    searchbtn=driver.find_element(By.XPATH,'//*[@id="gh-btn"]').click()
+    print ("Search button clicked.")
+    firstelementurl=driver.find_element(By.XPATH,'//*[@id="item2b45ac98b6"]/div/div[2]/a').get_attribute('href')
+    print(firstelementurl)
+    time.sleep(15)
+
+    url = firstelementurl
+
+    page = r.get(url)
+
+    soup = bs(page.content, "html.parser")
+
+    divTag = soup.find("div", {"class":"x-price-primary"})
+    # price = soup.find('span', {"class":"ux-textspans"})
+    # price = re.findall("$", divTag)
+
+
+    # price = [soup.find('span', {"class":"ux-textspans"}) for div in soup.find('div', {"class":"x-price-primary"})]
+
+    divStr = str(divTag)
+    divArray = divStr.split()
+
+    # divArray[5]
+
+    print(divArray[4][:4])
+
+ebay("macbook")
