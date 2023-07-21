8a74db555ec865afd88179581382385cc801eeb3
Harika <lakshmimaha683@gmail.com>
2023-07-21 09:10:05 +0530

    Updated files

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask, request, render_template
-
+from helper import flipkart
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('index.html')
 
-@app.route('/results', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def result():
-    searchData = request.form
-    return(searchData)
+    if request.method=="POST":
+        searchData = request.form["search"]
+        print(searchData)
+        flipkart(searchData)
+        return render_template('index.html')
+    # return(searchData)
 
 
 if __name__ =='__main__':
